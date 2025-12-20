be54b4aeab793762cf3042ccbb428e1c85827329
bala-amypo <bala@amypo.in>
2025-12-20 11:14:05 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,41 @@
 package com.example.demo.exception;
 
+import com.example.demo.dto.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse<Object>> handleResourceNotFound(ResourceNotFoundException ex) {
+
+        ApiErrorResponse<Object> response = new ApiErrorResponse<>();
+        response.setSuccess(false);
+        response.setMessage(ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ApiErrorResponse<Object>> handleValidationException(ValidationException ex) {
+
+        ApiErrorResponse<Object> response = new ApiErrorResponse<>();
+        response.setSuccess(false);
+        response.setMessage(ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse<Object>> handleGenericException(Exception ex) {
+
+        ApiErrorResponse<Object> response = new ApiErrorResponse<>();
+        response.setSuccess(false);
+        response.setMessage("Internal server error");
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
