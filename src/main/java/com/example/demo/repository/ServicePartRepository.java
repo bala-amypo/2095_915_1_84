14c27d5a1ad9d16e245a5a8a59cb26dda49ed83a
bala-amypo <bala@amypo.in>
2025-12-20 16:42:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServicePartRepository.java
+++ src/main/java/com/example/demo/repository/ServicePartRepository.java
@@ -2,5 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ServicePart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ServicePartRepository extends JpaRepository<ServicePart, Long> {}
+@Repository
+public interface ServicePartRepository extends JpaRepository<ServicePart, Long> {
+    
+}
