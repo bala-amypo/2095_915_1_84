e5f4ade0c1261ecc23ef87e66ffeaf1b3c9b52a6
bala-amypo <bala@amypo.in>
2025-12-20 11:38:23 +0000

    User commit

--- src/main/java/com/example/demo/servlet/VehicleServiceHistoryApplication.java
+++ src/main/java/com/example/demo/servlet/VehicleServiceHistoryApplication.java
@@ -1,12 +1,13 @@
-package com.example.demo;
+package com.example.demo.servlet;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@SpringBootApplication
-public class VehicleServiceHistoryApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(VehicleServiceHistoryApplication.class, args);
+public class SimpleHealthServlet extends HttpServlet {  // rename from VehicleServiceHistoryApplication
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("Service is running");
     }
 }
