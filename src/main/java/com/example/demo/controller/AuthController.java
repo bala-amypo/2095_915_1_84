cb36246f888402624120ed8d2cfe7f739fbd7c1d
bala-amypo <bala@amypo.in>
2025-12-21 05:05:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.AuthService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
