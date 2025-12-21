0598f8e676456cee9fbbcb60dea768cd72171fa3
bala-amypo <bala@amypo.in>
2025-12-21 08:20:34 +0000

    User commit

--- src/main/java/com/example/demo/service/ServiceEntryService.java
+++ src/main/java/com/example/demo/service/ServiceEntryService.java
@@ -1,9 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.model.ServiceEntry;
-import java.util.List;
 
 public interface ServiceEntryService {
-    ServiceEntry createServiceEntry(ServiceEntry entry);
-    List<ServiceEntry> getEntriesForVehicle(Long vehicleId);
+
+    ServiceEntry save(ServiceEntry entry);
 }
