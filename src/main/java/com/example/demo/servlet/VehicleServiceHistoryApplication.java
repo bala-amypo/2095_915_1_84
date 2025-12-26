cd42e4ac9ae7799c9b30664a0a1940d88864e1cc
bala-amypo <bala@amypo.in>
2025-12-26 17:29:42 +0000

    User commit

--- src/main/java/com/example/demo/servlet/VehicleServiceHistoryApplication.java
+++ src/main/java/com/example/demo/servlet/VehicleServiceHistoryApplication.java
@@ -1,15 +1,22 @@
-package com.example.demo;
+package com.example.demo.servlet;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@SpringBootApplication
-@ServletComponentScan
-public class VehicleServiceHistoryApplication {
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleServiceHistoryApplication.class, args);
-	}
-
+@WebServlet(urlPatterns = "/health", loadOnStartup = 1)
+public class SimpleHealthServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
+        resp.setContentType("text/plain");
+        PrintWriter writer = resp.getWriter();
+        writer.write("OK");
+        writer.flush();
+    }
 }
